e5dfadd2ac09c6811b34d7a6c52e4bdfc12d7a73
MikeGalli <mikegalligar@android-2a44d5b78463834e.seattleu.edu>
2016-09-29 19:15:30 -0700

    add animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+
+$(document).ready(function(){
+    $(".btn.btn-lg.btn-success").click(function(){
+        var x = $(".lead")
+        x.animate({left: '550px'});
+        x.animate({left: '0px'});
+    });
+});
